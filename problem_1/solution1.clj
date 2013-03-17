60d71ca481fd92b7b8573809e06903b7537ecf60
Christopher McCluskey <christopher.i.mccluskey@gmail.com>
2013-03-17 13:03:38 +0000

    Reorganised clojure file.
    Unaware of poorly structured script due to not flushing repl.

--- problem_1/solution1.clj
+++ problem_1/solution1.clj
@@ -1,19 +1,19 @@
-(defn solution-1
-	[n]
- 	(reduce + (filter multiple-of-3-or-5? (range n))))
+(defn is-rem-zero?
+	[dividend divisor]
+	(zero? (rem dividend divisor)))
 
 (defn multiple-of-5?
 	[n]
-	(isremzero? n 5))
+	(is-rem-zero? n 5))
 
 (defn multiple-of-3?
 	[n]
-	(isremzero? n 3))
+	(is-rem-zero? n 3))
 
 (defn multiple-of-3-or-5?
 	[n]
 	(or (multiple-of-3? n) (multiple-of-5? n)))
 
-(defn is-rem-zero?
-	[dividend divisor]
-	(zero? (rem dividend divisor)))
+(defn solution-1
+	[n]
+ 	(reduce + (filter multiple-of-3-or-5? (range n))))
